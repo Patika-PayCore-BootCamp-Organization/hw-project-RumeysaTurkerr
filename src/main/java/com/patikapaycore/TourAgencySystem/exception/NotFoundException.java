474933805a9d7c76e52884e23ab7aedadebf8bd7
Rümeysa Türker <91668320+RumeysaTurkerr@users.noreply.github.com>
2022-02-15 18:32:48 +0300

    Delete src directory

--- src/main/java/com/patikapaycore/TourAgencySystem/exception/NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.patikapaycore.TourAgencySystem.exception;
-
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message + " not found!");
-    }
-}
--- src/main/java/com/patikapaycore/TourAgencySystem/repository/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.patikapaycore.TourAgencySystem.repository;
-
-import com.patikapaycore.TourAgencySystem.model.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HotelRepository extends JpaRepository<Hotel,Integer> {
-}
